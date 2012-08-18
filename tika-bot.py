91d9a851780119967579bbae723dccba8270a771
Matthijs Kooijman <matthijs@stdin.nl>
2012-08-18 10:36:29 +0200

    Add initial tika-bot implementation
    This requires irclib 0.7, which is not included in the repository (but
    also not included with Debian at the time of writing, so has to be
    installed manually to make this bot work).

---
+++ tika-bot.py
@@ -0,0 +1,112 @@
+#! /usr/bin/env python
+# vim: set fileencoding=utf-8 :
+#
+# Simple IRC bot to receive notifications through XML-RCP and display them in a
+# channel.
+#
+# © Matthijs Kooijman <matthijs@stdin.nl>
+# Based on an example by Joel Rosdahl <joel@rosdahl.net>
+
+"""Notification IRC Bot
+
+This is a simple bot that joins a channel and then waits for notifications to
+be pushed through XML-RPC. These notifications will then be displayed in the
+proper channel.
+
+The known commands are:
+
+    stats -- Prints some channel information.
+
+    disconnect -- Disconnect the bot.  The bot will try to reconnect
+                  after 60 seconds.
+
+    die -- Let the bot cease to exist.
+"""
+
+import irc.bot
+from irc.client import irc_lower, ip_numstr_to_quad, ip_quad_to_numstr
+
+import xmlrpc
+
+class TikaBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, channel, nickname, server, port=6667):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        self.channel = channel
+
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+
+    def on_privmsg(self, c, e):
+        self.do_command(e, e.arguments()[0])
+
+    def on_pubmsg(self, c, e):
+        a = e.arguments()[0].split(":", 1)
+        if len(a) > 1 and irc_lower(a[0]) == irc_lower(self.connection.get_nickname()):
+            self.do_command(e, a[1].strip())
+        return
+
+    def do_command(self, e, cmd):
+        nick = e.source()
+        c = self.connection
+
+        if cmd == "disconnect":
+            self.disconnect()
+        elif cmd == "die":
+            self.die()
+        elif cmd == "stats":
+            for chname, chobj in self.channels.items():
+                c.notice(nick, "--- Channel statistics ---")
+                c.notice(nick, "Channel: " + chname)
+                users = chobj.users()
+                users.sort()
+                c.notice(nick, "Users: " + ", ".join(users))
+                opers = chobj.opers()
+                opers.sort()
+                c.notice(nick, "Opers: " + ", ".join(opers))
+                voiced = chobj.voiced()
+                voiced.sort()
+                c.notice(nick, "Voiced: " + ", ".join(voiced))
+        else:
+            c.notice(nick, "Not understood: " + cmd)
+
+    def say(self, s):
+        self.connection.privmsg(self.channel, s)
+
+def main():
+    import sys
+    if len(sys.argv) != 4:
+        print "Usage: python tika-bot.py <server[:port]> <channel> <nickname>"
+        sys.exit(1)
+
+    s = sys.argv[1].split(":", 1)
+    server = s[0]
+    if len(s) == 2:
+        try:
+            port = int(s[1])
+        except ValueError:
+            print "Error: Erroneous port."
+            sys.exit(1)
+    else:
+        port = 6667
+    channel = sys.argv[2]
+    nickname = sys.argv[3]
+
+    bot = TikaBot(channel, nickname, server, port)
+
+    # This spawns a new thread (that will automatically quit when the
+    # main thread quits). Note that no attempt is made to do locking at this
+    # moment, so it seems there's a chance that both threads are going to send
+    # a command at the same time, with all kinds of odd side effects. But, how
+    # likely is that?
+    rpc = xmlrpc.XMLRPC(bot)
+    rpc.daemon = True
+    rpc.start()
+
+    # This never returns
+    bot.start()
+
+if __name__ == "__main__":
+    main()
